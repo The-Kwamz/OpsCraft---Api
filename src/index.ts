bf07cc0ad1ef2f0ce941688eb72ceda98a4d9605
Kwame <kwameyinkah@gmail.com>
2026-03-30 10:41:07 +0200

    Add masked SQL connection string logging

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,16 @@ const globalForSql = globalThis as typeof globalThis & {
 async function getPool(): Promise<sql.ConnectionPool> {
   const connectionString = process.env.SQL_CONNECTION_STRING
 
+  console.log("SQL_CONNECTION_STRING exists:", !!connectionString)
+
+  if (connectionString) {
+    const masked = connectionString
+      .replace(/Password=[^;]*/i, "Password=***")
+      .replace(/Pwd=[^;]*/i, "Pwd=***")
+
+    console.log("SQL_CONNECTION_STRING masked:", masked)
+  }
+
   if (!connectionString) {
     throw new Error("SQL_CONNECTION_STRING is not set")
   }
